305e0ed935d1ae6ce34177e7cc787e6716e56e02
maxnhub <maxnikulnikov@gmail.com>
2019-12-01 15:41:21 +0300

    paths repaired

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
     const isProductionBuild = argv.mode === "production";
-    const publicPath = '/pitergsm';
+    const publicPath = '/pitergsm/';
 
     const pcss = {
         test: /\.(p|post|)css$/,
